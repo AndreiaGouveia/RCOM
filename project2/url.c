494247717b30aaee7037cbab18a9e034dcf1b1a5
filipasenra <filipasenra4@gmail.com>
2019-12-11 11:00:41 -0800

    Changing input format

--- project2/url.c
+++ project2/url.c
@@ -10,7 +10,7 @@ int parseURL(url* url, char * argv){
 
 // ftp://[<user>:<password>@]<host>/<url-path>
 
-if (sscanf(argv, "ftp://[%[^:]:%[^@]@]%[^/]/%s", url->user, url->password, url->host, url->path) != 4)
+if (sscanf(argv, "ftp://%[^:]:%[^@]@%[^/]/%s", url->user, url->password, url->host, url->path) != 4)
 {
     printf("Wrong Input!\n");
     return 1;
